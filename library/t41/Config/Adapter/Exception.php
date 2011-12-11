52ab59ede87700dcc0572176ad578cb6affa16bc
Olivier Lépine <olivier@quatrain.com>
2011-12-11 22:49:38 +0100

    Added Exception class for config adapters
    Change-Id: I66de4461a10c57f09efee07550c4e85c65ea66dc

---
+++ library/t41/Config/Adapter/Exception.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace t41\Config\Adapter;
+
+/**
+ * t41 Toolkit
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.t41.org/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@t41.org so we can send you a copy immediately.
+ *
+ * @category   t41
+ * @package    t41_Config
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ * @version    $Revision: 832 $
+ */
+
+/** Required files */
+require_once 't41/Config/Exception.php';
+
+/**
+ * Config Exception Class 
+ *
+ * @category   t41
+ * @package    t41_Config
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ */
+
+class Exception extends \t41\Config\Exception {
+	
+}
