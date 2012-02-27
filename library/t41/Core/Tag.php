bbca15b1356c4e0628b68b0f161b580e93857758
Olivier Lépine <olivier@quatrain.com>
2012-02-27 16:54:44 +0100

    Added first implementation of tags handling in model
    Change-Id: I5ff6abda9202921f5755b2ea2caaad19e8e9cf40

---
+++ library/t41/Core/Tag.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace t41\Core;
+
+/**
+ * t41 Toolkit
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.t41.org/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@t41.org so we can send you a copy immediately.
+ *
+ * @category   t41
+ * @package    t41_Core
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ * @version    $Revision: 972 $
+ */
+
+/**
+ *
+ * @category   t41
+ * @package    t41_Core
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ */
+class Tag {
+
+	/**
+	 * tag syntax: %store:val.sub%
+	 * 
+	 * ex: %env:date.today%
+	 */
+	
+	/**
+	 * Tag boundaries marker
+	 * @var string
+	 */
+	const MARKER = '%';
+	
+	/**
+	 * Tag store value separator
+	 * @var string
+	 */
+	const ENVSEP = ':';
+	
+	/**
+	 * Tag sub tag value separator
+	 * @var string
+	 */
+	const SUBSEP = '.';
+	
+	
+	/**
+	 * List of declared stores and their optional namespace
+	 * @var array
+	 */
+	static protected $_stores = array('env' => 't41');
+	
+	
+	static protected $_interface = 't41\Core\Tag\Interface';
+	
+
+	/**
+	 * Add or replace a tag store 
+	 * @param string $store
+	 * @param string $namespace
+	 */
+	static public function addStore($store, $namespace = 't41')
+	{
+		if (! is_string($store)) {
+			
+			throw new \t41\Exception("ARGUMENT_ILLEGAL_TYPEOF", array('string', $store));
+		}
+		
+		if (! is_string($namespace)) {
+				
+			throw new \t41\Exception("ARGUMENT_ILLEGAL_TYPEOF", array('string', $namespace));
+		}
+		
+		self::$_stores[$store] = $namespace;
+	}
+	
+	
+	/**
+	 * Return the stores array
+	 * @return array
+	 */
+	static public function getStores()
+	{
+		return self::$_stores;
+	}
+	
+	
+	static public function get($tag)
+	{
+		if (($ctag = self::_parseTag($tag)) == false) {
+			
+			return false;
+		}
+		
+		if (! is_array($ctag) || count($ctag) < 2) {
+			
+			return false;
+		}
+
+		/*
+		 * Send command to subclass, if available
+		 */
+		if (array_key_exists($ctag[0], self::$_stores)) {
+			
+			$class = sprintf('%s\Core\Tag\%sTag', self::$_stores[$ctag[0]], ucfirst(strtolower($ctag[0])));
+
+			if (! in_array(self::$_interface, (array) @class_implements($class))) {
+				
+				throw new \t41\Exception("CLASS_MISS_INTERFACE", array($class, self::$_interface));
+			}
+			
+			return $class::get($ctag[1], isset($ctag[2]) ? $ctag[2] : null);
+		
+		} else {
+			
+			return false;
+		}
+	}
+	
+	
+	/**
+	 * Extract relevant parameters from tag in string
+	 * @param string $tag
+	 * @return boolean|array
+	 */
+	static protected function _parseTag($tag)
+	{
+		if (substr($tag, 0, 1) != self::MARKER || substr($tag, -1) != self::MARKER) {
+			
+			return false;
+		}
+		
+		$pattern = sprintf('/%s([a-z]+)\\%s([a-z]+)\\%s*([a-z]*)%s/', self::MARKER, self::ENVSEP, self::SUBSEP, self::MARKER);
+		preg_match($pattern, $tag, $matches);
+		array_shift($matches);
+		return $matches;
+	}
+}
---
+++ library/t41/Core/Tag/EnvTag.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace t41\Core\Tag;
+
+/**
+ * t41 Toolkit
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.t41.org/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@t41.org so we can send you a copy immediately.
+ *
+ * @category   t41
+ * @package    t41_Core
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ * @version    $Revision: 972 $
+ */
+
+/**
+ *
+ * @category   t41
+ * @package    t41_Core
+ * @copyright  Copyright (c) 2006-2011 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ */
+class EnvTag implements TagInterface {
+
+	/**
+	 * Return environment value described by tag and optional sub tag
+	 * @param string $tag
+	 * @param string $sub
+	 * @return string
+	 */
+	static public function get($tag, $sub = null)
+	{
+		switch ($tag) {
+				
+			case 'date':
+				return self::getDateVal($sub);
+				break;
+		}
+	}
+	
+	
+	/**
+	 * Return a date calculated and formatted accordingly with parameters
+	 * @param string $when
+	 * @param string $pattern
+	 * @return string
+	 */
+	static public function getDateVal($when = null, $pattern = 'Y-m-d H:i:s')
+	{
+		$ref = time();
+	
+		switch ($when) {
+	
+			case 'yesterday':
+				$ref -= 86400;
+				break;
+	
+			case 'tomorrow':
+				$ref += 86400;
+				break;
+	
+			case 'today':
+			default:
+				break;
+		}
+	
+		return date($pattern, $ref);
+	}
+}
