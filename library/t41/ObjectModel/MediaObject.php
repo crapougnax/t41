0dae175321e80939723310eaf29f36422c763f2a
Olivier Lépine <olivier@quatrain.com>
2012-12-20 02:56:45 -0800

    Added some recent MS mimetypes.

--- library/t41/ObjectModel/MediaObject.php
+++ library/t41/ObjectModel/MediaObject.php
@@ -18,7 +18,18 @@ class MediaObject extends BaseObject {
 			'jpg'	=> 'image/jpeg',
 			'ttf'   => 'application/octet-stream',
 			'doc'	=> 'application/msword',
-			'pdf'	=> 'application/pdf'
+			'pdf'	=> 'application/pdf',
+			
+			'xlsx'	=> 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+			'xltx'	=> 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+			'potx'	=> 'application/vnd.openxmlformats-officedocument.presentationml.template',
+			'ppsx'	=> 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+			'pptx'	=> 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+			'sldx'	=> 'application/vnd.openxmlformats-officedocument.presentationml.slide',
+			'docx'	=> 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+			'dotx'	=> 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+			'xlam'	=> 'application/vnd.ms-excel.addin.macroEnabled.12',
+			'xlsb'	=> 'application/vnd.ms-excel.sheet.binary.macroEnabled.12'
 	);
 	
 	
