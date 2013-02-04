b2474fdef9a322d4a2c375e36dbdbd8d37f33de6
Olivier Lépine <olivier@quatrain.com>
2013-02-04 13:33:11 -0800

    Improved cache UI

--- controllers/CacheController.php
+++ controllers/CacheController.php
@@ -19,26 +19,29 @@ require_once 'Zend/Controller/Action.php';
 class t41_CacheController extends Zend_Controller_Action {
 
 	
+	protected $_cache;
 	
-	public function init() {
-		
+	
+	public function init()
+	{
+		$this->_cache = Core::cacheGetAdapter();
 	}
 	
 	
 	public function indexAction()
 	{
-		$cache = Core::cacheGetAdapter();
-		var_dump($cache->getFillingPercentage());
-		$html = '<table border="1"><tr><th>Id</th><th>Type</th><th>Date</th><th>TTL</th><th>Expire</th></tr>';
-		foreach ($cache->getIds() as $id) {
-			$metas = $cache->getMetadatas($id);
-			$html .= sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>'
+		//var_dump($cache->getFillingPercentage());
+		$html = '<table border="1"><tr><th>Id</th><th>Type</th><th>Date</th><th>TTL</th><th>Expire</th><th>&nbsp;</th></tr>';
+		foreach ($this->_cache->getIds() as $id) {
+			$metas = $this->_cache->getMetadatas($id);
+			$html .= sprintf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s | %s</td></tr>'
 							, $id
-							, $metas['tags'][0]
+							, implode(' ', $metas['tags'])
 							, date('d/m/Y H:i:s', $metas['mtime'])
 							, $metas['expire'] - $metas['mtime']
 							, date('d/m/Y H:i:s', $metas['expire'])
-					
+							, '<a href="/t41/cache/view/id/' . rawurlencode($id) . '">View</a>'
+							, '<a href="/t41/cache/delete/id/' . rawurlencode($id) . '">Delete</a>'
 					);
 		}
 		$html .= '</table>';
@@ -47,9 +50,27 @@ class t41_CacheController extends Zend_Controller_Action {
 	}
 	
 	
+	public function viewAction()
+	{
+		if (($cached = Core::cacheGet($this->_getParam('id'))) !== false) {
+			echo '<pre>' . print_r($cached,true) . '<pre>';
+		} else {
+			throw new \Exception("Unable to find a cached object with this id: " . $this->_getParam('id'));
+		}
+		exit();
+	}
+	
+	
+	public function deleteAction()
+	{
+		$this->_cache->remove($this->_getParam('id'));
+		$this->_redirect('/t41/cache');
+		exit();
+	}
+	
+	
 	public function cleanAction()
 	{
-		$cache = Core::cacheGetAdapter();
-		$cache->clean();
+		$this->_cache->clean();
 	}
 }
