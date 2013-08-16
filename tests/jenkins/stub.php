4fcfc63ca780a275b45b17a683e3d80d7a093564
Olivier Lépine <olivier@quatrain.com>
2013-08-16 02:19:24 -0700

    Ajout du stub.php pour création du .phar

---
+++ tests/jenkins/stub.php
@@ -0,0 +1,13 @@
+<?php
+Phar::mapPhar('myproject.phar');
+spl_autoload_register(function ($className) {
+	$libPath = 'phar://myproject.phar/lib/';
+	$classFile = str_replace('\\',DIRECTORY_SEPARATOR,$className).'.php';
+	$classPath = $libPath.$classFile;
+	if (file_exists($classPath)) {
+		require($classPath);
+	}
+});
+__HALT_COMPILER();
+
+?>
