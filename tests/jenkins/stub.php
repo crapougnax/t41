8779ab6a693aa4d7ae3f9540e098783954e43dc1
Olivier Lépine <olivier@quatrain.com>
2013-08-27 01:21:36 -0700

    modification et ajout des build phar et apidoc au bon endroit

--- tests/jenkins/stub.php
+++ tests/jenkins/stub.php
@@ -1,7 +1,7 @@
 <?php
 Phar::mapPhar('/var/lib/jenkins/jobs/t41/workspace/tests/rapports/myproject.phar');
 spl_autoload_register(function ($className) {
-	$libPath = 'phar://myproject.phar/lib/';
+	$libPath = 'phar://T41-${BUILD_NUMBER}.phar/lib/';
 	$classFile = str_replace('\\',DIRECTORY_SEPARATOR,$className).'.php';
 	$classPath = $libPath.$classFile;
 	if (file_exists($classPath)) {
