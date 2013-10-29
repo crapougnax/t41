7e1937c5202ced296138a250cad69820554c2da4
Olivier Lépine <olivier@quatrain.com>
2013-10-29 09:12:18 -0700

    minor commenting

--- library/t41/Backend/Adapter/AbstractPdoAdapter.php
+++ library/t41/Backend/Adapter/AbstractPdoAdapter.php
@@ -145,13 +145,13 @@ abstract class AbstractPdoAdapter extends AbstractAdapter {
 		}
 		
 		// Look for unsaved object in properties and save them @todo throw Exception in case of failure
-		if (self::$recursionSave === true) {
+//		if (self::$recursionSave === true) {
 			try {
 				$res = $this->_saveNewObjects($do);
 			} catch (\Exception $e) {
 				throw new Exception("Error Creating recursive record in $table : " . $e->getMessage());
 			}
-		}
+//		}
 			
 		// get a valid data array passing mapper if any
 		$recordSet = $this->_mapper ? $do->map($this->_mapper, $this) : $do->toArray($this);
