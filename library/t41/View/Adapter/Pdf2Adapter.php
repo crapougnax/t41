4a95a2834a83dfe15b7e0ca407e1bf90df0c59e3
Olivier Lépine <olivier@quatrain.com>
2013-12-18 05:14:36 -0800

    add new Pdf View Adapter using wkhtmltopdf

---
+++ library/t41/View/Adapter/Pdf2Adapter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace t41\View\Adapter;
+
+/**
+ * t41 Toolkit
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.t41.org/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@t41.org so we can send you a copy immediately.
+ *
+ * @category   t41
+ * @package    t41_View
+ * @copyright  Copyright (c) 2006-2012 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ */
+
+use t41\Parameter;
+
+/**
+ * Class providing the view engine with a Pdf context adapter.
+ *
+ * @category   t41
+ * @package    t41_View
+ * @copyright  Copyright (c) 2006-2013 Quatrain Technologies SARL
+ * @license    http://www.t41.org/license/new-bsd     New BSD License
+ */
+class Pdf2Adapter extends WebAdapter {
+
+	
+	const ID = 'Pdf2';
+	
+	
+	protected $_context = 'Pdf2';
+	
+	
+	
+	public function __construct(array $parameters = array())
+	{
+		$parameters['orientation'] 	= new Parameter(Parameter::STRING, PdfAdapter::ORIENTATION_PORTRAIT, false, array(PdfAdapter::ORIENTATION_PORTRAIT, PdfAdapter::ORIENTATION_LANDSCAPE));
+		$parameters['copies']		= new Parameter(Parameter::INTEGER, 1);
+		$parameters['title'] 		= new Parameter(Parameter::STRING);
+		
+		parent::__construct($parameters);
+	}
+	
+
+    protected function _render()
+    {
+    	$command = 'wkhtmltopdf ';
+    	if ($this->getParameter('orientation') == PdfAdapter::ORIENTATION_LANDSCAPE) {
+    		$command .= '-O Landscape';
+    	}
+    	
+    	$dir = '/dev/shm/';
+    	$html = parent::_render();
+    	
+    	$key = hash('md5', $html);
+    	
+    	file_put_contents($dir . $key . '.html', $html);
+    	
+    	exec(sprintf("%s %s%s.html %s%s.pdf", $command, $dir, $key, $dir, $key));
+    	
+    	header('Content-Type: application/pdf');
+    	header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
+    	header('Pragma: public');
+    	header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+    	header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+    	
+    	$doc = $this->getParameter('title') ? str_replace('/', '-', $this->getParameter('title')) . '.pdf' : 'Export.pdf';
+    	 
+    	header('Content-Disposition: inline; filename="' . $doc . '";');
+    	echo file_get_contents($dir . $key . '.pdf');
+    	unlink($dir . $key . '.*');
+    }
+}
