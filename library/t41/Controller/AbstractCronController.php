7cc0192388ae28fabe7516cbc19e937a259b6b8c
Olivier Lépine <olivier@quatrain.com>
2014-05-12 14:21:55 +0200

    improved abstract cron controller

--- library/t41/Controller/AbstractCronController.php
+++ library/t41/Controller/AbstractCronController.php
@@ -2,8 +2,26 @@
 
 namespace t41\Controller;
 
+use t41\Core;
+
 class AbstractCronController extends \Zend_Controller_Action {
 
+	
+	public function preDispatch()
+	{
+		if (Core::$mode != 'cli') {
+			echo '<html><meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+			echo '<body><pre>';
+		}
+	}
+	
+	
+	public function postDispatch()
+	{
+		if (Core::$mode != 'cli') {
+			echo "</pre></body></html>\n";
+		}
+	}
 
 	public function hourlyAction()
 	{
@@ -20,5 +38,12 @@ class AbstractCronController extends \Zend_Controller_Action {
 	public function weeklyAction()
 	{
 		return 'SKIPPED';
+	}
+	
+	
+	protected function _getTS()
+	{
+		$date = new \DateTime();
+		return $date->format('Y-m-d H:i:s');
 	}
 }
