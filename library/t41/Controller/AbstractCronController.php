623b2e493a4f0d1077aadd9171b6a5f7023c1b3c
Olivier Lépine <olivier@quatrain.com>
2014-05-20 14:27:46 +0200

    improved abstract Cron controller

--- library/t41/Controller/AbstractCronController.php
+++ library/t41/Controller/AbstractCronController.php
@@ -3,9 +3,13 @@
 namespace t41\Controller;
 
 use t41\Core;
+use t41\View;
 
 class AbstractCronController extends \Zend_Controller_Action {
 
+	
+	protected $_component;
+	
 	
 	public function preDispatch()
 	{
@@ -16,10 +20,34 @@ class AbstractCronController extends \Zend_Controller_Action {
 	}
 	
 	
+	public function indexAction()
+	{
+		if (Core::$env != Core::ENV_DEV) {
+			exit("Access denied");
+		}
+		
+		echo '<h1>Available actions</h1>';
+		$methods = get_class_methods($this);
+		foreach ($methods as $method) {
+			if (substr($method, -6) == 'Action' && substr($method,0,5) != 'index') {
+				printf('<a href="/%s/%s/%s">%s</a><br/>'
+						, $this->_getParam('module')
+						, $this->_getParam('controller')
+						, strtolower(str_replace('Action', '', $method))
+						, $method
+				);
+			}
+		}
+	}
+	
+	
 	public function postDispatch()
 	{
 		if (Core::$mode != 'cli') {
 			echo "</pre></body></html>\n";
+		} else {
+			$this->_component->register();
+			echo View::display();
 		}
 	}
 
